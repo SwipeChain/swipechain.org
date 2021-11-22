d01104070c6d9cdef8a486fc52248298ffd54931
easechain <93298685+easechain@users.noreply.github.com>
2021-11-22 12:59:52 -0800

    add pdf upload

--- sources/gulp_tasks/serve.js
+++ sources/gulp_tasks/serve.js
@@ -1,13 +1,23 @@
 import gulp from "gulp";
 
-gulp.task("serve", () =>
-    new Promise((res, rej) => {
-        $.browsersync.create();
-        $.browsersync.init({
-            server: "./"+$.path.dest,
-            tunnel: false,
-            port: 9000
-        });
-        res();
-    })
+gulp.task("serve", () => {
+        gulp
+        .src(["./src/pdf/Whitepaper.pdf", "./src/Whitepaper.pdf"])
+        .pipe($.gulp.dest($.path.dest))
+        .pipe(
+            $.debug({
+            title: "static doc",
+            })
+        );
+
+        return new Promise((res, rej) => {
+            $.browsersync.create();
+            $.browsersync.init({
+                server: "./"+$.path.dest,
+                tunnel: false,
+                port: 9000
+            });
+            res();
+        })
+    }
 );
